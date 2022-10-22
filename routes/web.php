b044ea5caedba1aadb464744ed85883d95d82bcf
lai yu tien <s3a932004@student.ncut.edu.tw>
2022-10-23 01:55:26 +0800

    6、7: 產生 Controller、Route 串接 Controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -55,3 +56,6 @@ Route::group(['prefix' => 'admin'], function(){
        return 'admin dashboard';
    });
 });
+
+//7.
+Route::get('home', [HomeController::class, 'index'])->name('home.index');
